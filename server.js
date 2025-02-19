06431c5ee27e2df36d4b22e1213c88ee5ea9fa0f
wizbenny <rhadonizer@gmail.com>
2025-02-19 16:23:55 -0600

    asdads

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ const authenticateToken = (req, res, next) => {
   });
   */
 
-  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+  jwt.verify(token, process.env.aVeryStrongSecretKeyHere, (err, user) => {
     if (err) {
       console.log("Token verification failed:", err);
       return res.status(403).json({ message: "Invalid token" });
