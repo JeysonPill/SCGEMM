b7107e3688986dc3022404973b79990c2497e831
BennyBenn <rhadonizer@gmail.com>
2025-05-07 22:59:22 -0600

    asddsa

--- db_init.sql
+++ db_init.sql
@@ -1,6 +1,6 @@
 -- Drop and recreate database
 --DROP DATABASE IF EXISTS SCGEM;
-CREATE DATABASE SCGEM CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE SCGEM ;
 
 -- Drop and recreate user
 DROP USER IF EXISTS 'node_user'@'localhost';
