af2f0c5fe9978d65a25a82615987c242dd1daf97
BennyBenn <rhadonizer@gmail.com>
2025-05-07 23:10:02 -0600

    asdasd

--- db_init.sql
+++ db_init.sql
@@ -60,7 +60,7 @@ CREATE TABLE USUARIOS (
     user_name VARCHAR(30),
     password VARCHAR(100) UNIQUE NOT NULL,
     id_user VARCHAR(5) PRIMARY KEY,
-    user_role VARCHAR(1)
+    user_role VARCHAR(2)
 );
 
 -- HORARIOS
