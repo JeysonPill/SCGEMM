e8bda6556ccc90e63efd25cdcd54fae8ecbb9254
JeysonPill <caceresjeyson8@gmail.com>
2025-06-11 20:49:33 -0600

    zdasd

--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,7 @@
 // routes/admin.js
 const express = require('express');
 const router = express.Router();
-const db = require('../config/db'); 
+//const db = require('../config/db'); 
 const jwt = require('jsonwebtoken');
 
 
